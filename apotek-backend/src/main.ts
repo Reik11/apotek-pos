93bdb6599c1ec57ef6d553b61548a11d1bc57d18
Reik11 <xireikikuromori@gmail.com>
2026-06-10 06:01:12 +0700

    Initial commit

--- apotek-backend/src/main.ts
+++ apotek-backend/src/main.ts
@@ -4,11 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Aktifkan CORS (nanti dibutuhkan Flutter)
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`🚀 Server berjalan di http://localhost:${port}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Server berjalan di port ${port}`);
 }
 bootstrap();
