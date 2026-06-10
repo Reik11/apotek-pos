1150e453f27a5027e34d70f3d1bc4d50b4a75d3b
Reik11 <xireikikuromori@gmail.com>
2026-06-10 23:13:27 +0700

    fix: remove --no-wasm flag

--- apotek_web/netlify-build.sh
+++ apotek_web/netlify-build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-git clone https://github.com/flutter/flutter.git --depth 1 -b 3.41.6 flutter
+set -e
+
+echo "=== Installing Flutter ==="
+git clone https://github.com/flutter/flutter.git flutter
+cd flutter
+git checkout 3.41.6
+cd ..
 export PATH="$PATH:`pwd`/flutter/bin"
 
+echo "=== Flutter Version ==="
 flutter --version
+
+echo "=== Enable Web ==="
+flutter config --enable-web
+
+echo "=== Get Dependencies ==="
 flutter pub get
-flutter build web --release --no-wasm
+
+echo "=== Build Web ==="
+flutter build web --release
